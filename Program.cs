465f6ebf51798f3b658a306fa9724f280533708f
Duc.NT <311020ntd@gmail>
2024-08-12 20:33:53 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("*");
+}
+for (int i = 7; i >= 1; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+
+    }
+    Console.WriteLine("*");
+}
